1818197461a3e4bcc97a11935f8dd17ebdfd422a
Axel <axelmercado126@gmail.com>
2026-01-22 10:27:13 -0600

    Estructura inicial de home, correccion de logo,  navegación y páginas de encuestas y testimonios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Footer } from "@/components/layout/Footer";
 import { Hero } from "@/components/sections/Hero";
 import { About } from "@/components/sections/About";
 import { Services } from "@/components/sections/Services";
+import { Testimonials } from "@/components/sections/Testimonials";
 import { Contact } from "@/components/sections/Contact";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
                 <Hero />
                 <About />
                 <Services />
+                <Testimonials />
                 <Contact />
             </main>
             <Footer />
